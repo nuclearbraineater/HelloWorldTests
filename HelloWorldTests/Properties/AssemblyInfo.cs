22aae76c345671e999690764e914279c584d0718
mantis <nuclearbraineater@gmail.com>
2018-09-28 12:36:29 +0200

    NEW | UnitTest | Initial commit

---
+++ HelloWorldTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWorldTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloWorldTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7f5f8874-0394-4fd5-ad10-db23e1e3a892")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HelloWorld;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var printHello = new PrintHello();
+
+            //Act
+            var result = printHello.PrintHelloWorld();
+
+            //Assert
+            Assert.AreEqual("HelloWorld", result);
+        }
+    }
+}
